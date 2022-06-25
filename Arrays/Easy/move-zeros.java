bdb3883ac7063c5460589f704d2512657824e179
Triasa Munshi <79649967+triasa5@users.noreply.github.com>
2022-06-25 18:18:12 +0530

    Update move-zeros.java

--- Arrays/Easy/move-zeros.java
+++ Arrays/Easy/move-zeros.java
@@ -3,9 +3,9 @@ class Solution {
         int z = 0;
         for (int i = 0; i < nums.length; i++) {
             if(nums[i] != 0) 
-                nums[z++] = nums[i];
+                nums[z++] = nums[i];  // write the non zero elements at the beginning
         }
         while (z < nums.length) 
-            nums[z++] = 0;
+            nums[z++] = 0; // fill the rest of the position with zeros
     }
 }
