cbf56029d89b039048d58a219de47b83e420e8dc
Triasa Munshi <79649967+triasa5@users.noreply.github.com>
2022-06-26 11:56:33 +0530

    Update two-sum.java

--- Arrays/Easy/two-sum.java
+++ Arrays/Easy/two-sum.java
@@ -29,7 +29,7 @@ Constraints:
 -109 <= target <= 109
 Only one valid answer exists.
 **/
-
+// Time complexity: O(n2)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
@@ -45,5 +45,18 @@ class Solution {
         return result;
     }
 }
-
-// Time complexity: O(n2)
+// Time complexity: O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int diff = 0;
+        for (int i = 0;i <nums.length; i++) {
+            diff = target - nums[i];
+            if (map.containsKey(diff) == true) {
+                return new int[] {map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {-1, -1};
+    }
+}
